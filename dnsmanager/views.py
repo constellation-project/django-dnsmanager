e7ec79c68934c559a217542d9ca4c338cab996ca
Alexandre Iooss <erdnaxe@crans.org>
2019-12-16 16:40:04 +0100

    Fix import order

--- dnsmanager/views.py
+++ dnsmanager/views.py
@@ -1,5 +1,5 @@
-from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic.detail import DetailView
 
 from .models import Zone
 
