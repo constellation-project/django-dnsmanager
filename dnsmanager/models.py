0c8cfc2b267de1fe264627cee37a49e3b7853912
Bombar Maxime <bombar@crans.org>
2019-12-21 11:25:35 +0100

    Fix import statements order.

--- dnsmanager/models.py
+++ dnsmanager/models.py
@@ -2,8 +2,8 @@ from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator, \
     RegexValidator
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 from polymorphic.models import PolymorphicModel
 
 # Custom validator to check a domain name
