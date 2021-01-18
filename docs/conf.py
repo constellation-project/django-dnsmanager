28a27916b14a6838981c2bd250a2707a22d37dcd
Alexandre Iooss <erdnaxe@crans.org>
2021-01-18 18:29:42 +0100

    Add sphinx_rtd_theme extension to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'sphinxcontrib_django',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
